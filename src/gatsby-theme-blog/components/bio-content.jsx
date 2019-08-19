ab60b01361e3ddcc46ec7ba81f01f6b75bbf0a01
Victoria Kaihe <tori.ellen.g.k@gmail.com>
2019-08-20 11:21:46 +1200

    fix some bullshit that was missing from the tutorial

---
+++ src/gatsby-theme-blog/components/bio-content.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/no-unescaped-entities */
+import React, { Fragment } from 'react';
+
+export default () => (
+  <Fragment>
+    This is my updated bio.
+    <br />
+    It's shadowing the content from the theme.
+  </Fragment>
+);
